0e90170fd7068f2c03dadb58896afafaf8e07807
me <discostar711@gmail.com>
2019-05-19 17:17:54 +0100

    Initial server commit

---
+++ server/src/main/java/com/rajewski/jobfinder/webapp/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.rajewski.jobfinder.webapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.rajewski.jobfinder.webapp")
+public class AppConfig {
+}
