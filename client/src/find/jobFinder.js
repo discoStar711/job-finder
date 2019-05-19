0e973a60b9d58b9b26e6862523349e6907067735
me <discostar711@gmail.com>
2019-05-19 17:31:29 +0100

    Fetch jobs from server API

--- client/src/find/jobFinder.js
+++ client/src/find/jobFinder.js
@@ -3,6 +3,8 @@ import FindForm from './findForm';
 import FindResult from './findResult';
 import axios from 'axios';
 
+const API_URL = 'http://localhost:8080/find/job?keywords=';
+
 class JobFinder extends Component {
 
     constructor(props) {
@@ -11,6 +13,23 @@ class JobFinder extends Component {
             input: '',
             resultList: 'no results'
         };
+        this.findJobs = this.findJobs.bind(this);
+    }
+
+    findJobs(input) {
+        const query = API_URL + input;
+        axios.get(query)
+            .then(response => {
+                const responseString = JSON.stringify(response);
+                this.setState({
+                    resultList: responseString
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    resultList: 'Something went wrong. Please try again.'
+                });
+            });
     }
 
     render() {
@@ -19,7 +38,7 @@ class JobFinder extends Component {
                 className="form-container"
             >
                 <FindForm
-                    onFormSubmit={}
+                    onFormSubmit={this.findJobs}
                 />
                 <FindResult
                     results={this.state.resultList}
