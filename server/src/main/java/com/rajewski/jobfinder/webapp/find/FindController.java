131abf6a39bfd6174f5a0cddd6a0dafaf8a84e80
me <discostar711@gmail.com>
2019-05-19 17:56:12 +0100

    Add /job endpoint to return jobs by keyword

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/FindController.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/FindController.java
@@ -4,4 +4,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/find")
-public class FindController {}
+public class FindController {
+
+    @GetMapping("/job")
+    public String getJobsBy(@RequestParam String keyword) {
+        return keyword;
+    }
+}
