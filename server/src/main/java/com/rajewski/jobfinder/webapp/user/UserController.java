4d6adec218515019e46bc4f44edf67f0401559b2
me <discostar711@gmail.com>
2019-05-20 23:11:36 +0100

    Add /new endpoint to save user to database

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserController.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserController.java
@@ -1,9 +1,15 @@
 package com.rajewski.jobfinder.webapp.user;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @PostMapping("/new")
+    public void saveUser(HttpEntity<String> httpEntity) {
+        UserManager manager = new UserManager();
+        manager.save(httpEntity);
+    }
 }
