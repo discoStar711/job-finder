37810940d1216d014973a2328cbdeb6cf7ed10a9
me <discostar711@gmail.com>
2019-05-20 22:53:22 +0100

    Save user to database with encoded password

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserDao.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserDao.java
@@ -1,6 +1,8 @@
 package com.rajewski.jobfinder.webapp.user;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -13,5 +15,17 @@ public class UserDao {
         dataSource.setUsername("root");
         dataSource.setPassword("password");
         return dataSource;
+    }
+
+    public void save(User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        String email = user.getEmail();
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        String encodedPassword = encoder.encode(password);
+        String query = "INSERT INTO user (name, password, email) VALUES('" + username + "','" + encodedPassword + "','" + email + "');";
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
+        jdbcTemplate.execute(query);
     }
 }
