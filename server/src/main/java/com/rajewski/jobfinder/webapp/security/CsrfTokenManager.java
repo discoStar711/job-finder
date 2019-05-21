514c09be9b3803db51a63c1619b3b62b9daf1b75
me <discostar711@gmail.com>
2019-05-21 20:32:50 +0100

    Create CSRF token

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfTokenManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfTokenManager.java
@@ -6,4 +6,10 @@ import java.util.Set;
 public class CsrfTokenManager {
 
     private static Set<String> loginCsrfTokens = new HashSet<>();
+
+    private String createToken() {
+        CsrfToken csrfToken = new CsrfToken();
+        String token = csrfToken.create();
+        return token;
+    }
 }
