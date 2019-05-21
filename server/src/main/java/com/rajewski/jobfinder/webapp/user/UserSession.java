4d3c45628bdacb6a956a143a31502b3002c00127
me <discostar711@gmail.com>
2019-05-21 22:54:11 +0100

    Add UserSession

---
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSession.java
@@ -0,0 +1,20 @@
+package com.rajewski.jobfinder.webapp.user;
+
+public class UserSession {
+
+    private Integer userId;
+    private String sessionCsrfToken;
+
+    public UserSession(Integer userId, String sessionCsrfToken) {
+        this.userId = userId;
+        this.sessionCsrfToken = sessionCsrfToken;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getSessionCsrfToken() {
+        return sessionCsrfToken;
+    }
+}
