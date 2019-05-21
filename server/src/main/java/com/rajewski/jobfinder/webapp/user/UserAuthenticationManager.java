46b86bd976bcc0842f0a5628ca1efe34f10e4734
me <discostar711@gmail.com>
2019-05-21 22:26:44 +0100

    Add authenticate() method to authenticate login credentials

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserAuthenticationManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserAuthenticationManager.java
@@ -1,5 +1,14 @@
 package com.rajewski.jobfinder.webapp.user;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class UserAuthenticationManager {
 
+    public ResponseEntity<String> authenticate(HttpServletRequest request, HttpServletResponse response, HttpEntity<String> httpEntity) {
+
+    }
 }
