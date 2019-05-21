5ebfbc555c81b07deef06db86e7d8b543d19d7a0
me <discostar711@gmail.com>
2019-05-21 20:30:22 +0100

    Create CSRF token set for login form

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfTokenManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfTokenManager.java
@@ -1,5 +1,9 @@
 package com.rajewski.jobfinder.webapp.security;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CsrfTokenManager {
 
+    private static Set<String> loginCsrfTokens = new HashSet<>();
 }
