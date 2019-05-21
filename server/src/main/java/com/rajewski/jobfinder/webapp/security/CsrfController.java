9041ef5e7252f8d9ff05e883150c914f39edd478
me <discostar711@gmail.com>
2019-05-21 21:32:19 +0100

    Enable CORS on '/csrfLogin' endpoint

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfController.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfController.java
@@ -1,11 +1,13 @@
 package com.rajewski.jobfinder.webapp.security;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CsrfController {
 
+    @CrossOrigin(origins = "http://localhost:3000", allowedHeaders = {"Origin", "X-Requested-With", "Content-Type", "Accept",  "Access-Control-Allow-Origin"})
     @GetMapping("/csrfLogin")
     public String getCsrfLoginToken() {
         CsrfTokenManager manager = new CsrfTokenManager();
