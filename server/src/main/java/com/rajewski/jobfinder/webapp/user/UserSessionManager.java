092403dddb0cf4a3f1a7fe03b42175b99a436b87
me <discostar711@gmail.com>
2019-05-21 23:15:28 +0100

    Add registerSession() to register user session in memory

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSessionManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSessionManager.java
@@ -6,4 +6,8 @@ import java.util.Map;
 public class UserSessionManager {
 
     private static Map<String, UserSession> sessions = new HashMap<>();
+
+    public void registerSession(String sessionId, UserSession userSession) {
+        sessions.put(sessionId, userSession);
+    }
 }
