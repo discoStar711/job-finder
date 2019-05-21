ee0091910f4f1bd6e2574a46a936862922db625c
me <discostar711@gmail.com>
2019-05-22 00:29:31 +0100

    Add isSessionValid() to find session id and compare CSRF tokens

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSessionManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSessionManager.java
@@ -10,4 +10,18 @@ public class UserSessionManager {
     public void registerSession(String sessionId, UserSession userSession) {
         sessions.put(sessionId, userSession);
     }
+
+    public static boolean isSessionValid(String sessionId, String csrfToken) {
+        UserSession userSession = sessions.get(sessionId);
+
+        if (userSession != null) {
+            if (userSession.getSessionCsrfToken().equals(csrfToken)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
