ee151b3ffd58957dbd85de0df6d19e8dfd517663
me <discostar711@gmail.com>
2019-05-21 23:13:08 +0100

    Add HashMap to store session id and UserSession object pairs

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSessionManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserSessionManager.java
@@ -1,5 +1,9 @@
 package com.rajewski.jobfinder.webapp.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserSessionManager {
 
+    private static Map<String, UserSession> sessions = new HashMap<>();
 }
