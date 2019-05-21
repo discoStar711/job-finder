488dfe72d876499c3c71e04ff4d18fabdb3ade0f
me <discostar711@gmail.com>
2019-05-21 19:13:01 +0100

    Add UserDetails component

---
+++ client/src/user/userDetails.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class UserDetails extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: ''
+        };
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default UserDetails;
