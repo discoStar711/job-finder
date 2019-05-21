3a78a936fc1ed8bfb520500c75e9b23329545cfc
me <discostar711@gmail.com>
2019-05-21 23:27:37 +0100

    Add '/auth' endpoint to post and authenticate login credentials

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserController.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserController.java
@@ -1,11 +1,21 @@
 package com.rajewski.jobfinder.webapp.user;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @PostMapping("/auth")
+    public ResponseEntity<String> loginAuthenticate(HttpServletRequest request, HttpServletResponse response, HttpEntity<String> httpEntity) {
+        UserAuthenticationManager manager = new UserAuthenticationManager();
+        return manager.authenticate(request, response, httpEntity);
+    }
 
     @CrossOrigin
     @PostMapping("/new")
