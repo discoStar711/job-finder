fcef9f41b1e0463719bf572ee86e31a44bf10884
me <discostar711@gmail.com>
2019-05-21 20:28:28 +0100

    Create random UUID

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfToken.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfToken.java
@@ -1,5 +1,11 @@
 package com.rajewski.jobfinder.webapp.security;
 
+import java.util.UUID;
+
 public class CsrfToken {
 
+    public String create() {
+        String token = UUID.randomUUID().toString();
+        return token;
+    }
 }
