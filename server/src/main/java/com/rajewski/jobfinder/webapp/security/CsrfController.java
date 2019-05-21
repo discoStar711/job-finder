d10042747f0c609c9400bdd395eb666aa010cc31
me <discostar711@gmail.com>
2019-05-21 21:29:10 +0100

    Add '/csrfLogin' endpoint to return CSRF token

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfController.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/CsrfController.java
@@ -1,5 +1,14 @@
 package com.rajewski.jobfinder.webapp.security;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CsrfController {
 
+    @GetMapping("/csrfLogin")
+    public String getCsrfLoginToken() {
+        CsrfTokenManager manager = new CsrfTokenManager();
+        return manager.getLoginToken();
+    }
 }
