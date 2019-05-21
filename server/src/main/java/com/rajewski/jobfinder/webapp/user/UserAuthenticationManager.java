dccad015c9c3e7cd953f270e1c3465dae880a4c7
me <discostar711@gmail.com>
2019-05-21 22:38:33 +0100

    Check that login CSFR token was generated

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserAuthenticationManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserAuthenticationManager.java
@@ -1,6 +1,8 @@
 package com.rajewski.jobfinder.webapp.user;
 
+import com.rajewski.jobfinder.webapp.security.CsrfTokenManager;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,5 +12,13 @@ public class UserAuthenticationManager {
 
     public ResponseEntity<String> authenticate(HttpServletRequest request, HttpServletResponse response, HttpEntity<String> httpEntity) {
 
+        String csrfToken = request.getHeader("CSRF-Token");
+        CsrfTokenManager csrfTokenManager = new CsrfTokenManager();
+
+        if (csrfTokenManager.containsLoginCsrfToken(csrfToken)) {
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
     }
 }
