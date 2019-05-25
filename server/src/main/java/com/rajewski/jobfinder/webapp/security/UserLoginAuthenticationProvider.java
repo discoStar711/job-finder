a017cc73f6e60ce211b1bc8aa9aed603917040bb
me <discostar711@gmail.com>
2019-05-25 18:52:44 +0100

    Check if authentication object/token is supported

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationProvider.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationProvider.java
@@ -1,6 +1,7 @@
 package com.rajewski.jobfinder.webapp.security;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
@@ -9,10 +10,20 @@ public class UserLoginAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
+        if (supports(authentication.getClass())) {
+            
+        } else {
+            throw new AuthenticationServiceException("Could not authenticate.");
+        }
     }
 
     @Override
     public boolean supports(Class<?> aClass) {
 
+        if (aClass.equals(UserLoginAuthenticationToken.class)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
