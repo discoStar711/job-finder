bce177bd8cd49e7d5a02291f7b370d750edaea5f
me <discostar711@gmail.com>
2019-05-25 18:34:02 +0100

    Add interface methods

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationToken.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationToken.java
@@ -4,7 +4,17 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 
 public class UserLoginAuthenticationToken extends AbstractAuthenticationToken {
 
-    public UserLoginAuthenticationToken() {
+    public UserLoginAuthenticationToken(String csrfToken, String credentials) {
         super(null);
+    }
+
+    @Override
+    public String getCredentials() {
+        return "";
+    }
+
+    @Override
+    public String getPrincipal() {
+        return "";
     }
 }
