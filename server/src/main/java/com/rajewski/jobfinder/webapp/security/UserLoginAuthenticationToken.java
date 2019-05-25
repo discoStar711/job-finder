7541330cb7032cf100eb98d8746f56e72b2168ae
me <discostar711@gmail.com>
2019-05-25 18:29:49 +0100

    Add UserLoginAuthenticationToken to represent authentication request or principal

---
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationToken.java
@@ -0,0 +1,7 @@
+package com.rajewski.jobfinder.webapp.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+public class UserLoginAuthenticationToken extends AbstractAuthenticationToken {
+    
+}
