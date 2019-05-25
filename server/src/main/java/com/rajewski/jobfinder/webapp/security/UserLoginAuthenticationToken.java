c1b2a767883bf35bd5c8af7b00ef13fc4861c552
me <discostar711@gmail.com>
2019-05-25 18:30:40 +0100

    Add default constructor

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationToken.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginAuthenticationToken.java
@@ -3,5 +3,8 @@ package com.rajewski.jobfinder.webapp.security;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 
 public class UserLoginAuthenticationToken extends AbstractAuthenticationToken {
-    
+
+    public UserLoginAuthenticationToken() {
+        super(null);
+    }
 }
