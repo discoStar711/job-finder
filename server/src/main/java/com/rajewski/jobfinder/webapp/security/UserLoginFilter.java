c414d39e6c7f73852cdc5c4213020096b947ad3d
me <discostar711@gmail.com>
2019-05-25 18:11:35 +0100

    Add constructor to pass RequestMatcher to parent class

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginFilter.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/UserLoginFilter.java
@@ -1,7 +1,11 @@
 package com.rajewski.jobfinder.webapp.security;
 
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 public class UserLoginFilter extends AbstractAuthenticationProcessingFilter {
-    
+
+    public UserLoginFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
 }
