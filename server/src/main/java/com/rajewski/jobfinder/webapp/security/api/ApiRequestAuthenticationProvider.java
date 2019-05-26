ddf149f11fa69ba270fba8f97bcfae4d9b819d03
me <discostar711@gmail.com>
2019-05-26 13:49:29 +0100

    Check if Authentication token is supported

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/api/ApiRequestAuthenticationProvider.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/api/ApiRequestAuthenticationProvider.java
@@ -1,6 +1,7 @@
 package com.rajewski.jobfinder.webapp.security.api;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
@@ -9,10 +10,20 @@ public class ApiRequestAuthenticationProvider implements AuthenticationProvider 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
+        if (supports(authentication.getClass())) {
+
+        } else {
+            throw new AuthenticationServiceException("Could not authenticate session or CSRF token.");
+        }
     }
 
     @Override
     public boolean supports(Class<?> aClass) {
 
+        if (aClass.equals(ApiRequestAuthenticationToken.class)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
