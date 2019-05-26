c1cb3b242012fffadef46e952ee15ff17f48d5ad
me <discostar711@gmail.com>
2019-05-26 13:36:46 +0100

    Add CSRF token field and getter

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/api/ApiRequestAuthenticationToken.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/api/ApiRequestAuthenticationToken.java
@@ -4,8 +4,14 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 
 public class ApiRequestAuthenticationToken extends AbstractAuthenticationToken {
 
+    private String csrfToken;
+
     public ApiRequestAuthenticationToken() {
         super(null);
+    }
+
+    public String getCsrfToken() {
+        return csrfToken;
     }
 
     @Override
