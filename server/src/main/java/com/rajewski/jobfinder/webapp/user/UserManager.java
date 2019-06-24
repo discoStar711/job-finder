5c0ec202ca6d17713d7a94fc20cd1925e8acb649
me <discostar711@gmail.com>
2019-06-24 16:41:48 +0100

    Check if user with given username is registered in database and return response

--- server/src/main/java/com/rajewski/jobfinder/webapp/user/UserManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/user/UserManager.java
@@ -2,22 +2,30 @@ package com.rajewski.jobfinder.webapp.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class UserManager {
 
-    public void save(HttpEntity<String> httpEntity) {
+    public ResponseEntity<String> save(HttpEntity<String> httpEntity) {
         String userDetails = httpEntity.getBody();
         try {
             ObjectMapper mapper = new ObjectMapper();
             User user = mapper.readValue(userDetails, User.class);
 
-            UserDao userDao = new UserDao();
-            userDao.save(user);
+            if (isRegistered(user)) {
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            } else {
+                UserDao userDao = new UserDao();
+                userDao.save(user);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -29,6 +37,12 @@ public class UserManager {
         Integer userId = UserSessionManager.getSession(sessionId.getValue()).getUserId();
         UserDao userDao = new UserDao();
         return userDao.findUserById(userId);
+    }
+
+    private boolean isRegistered(User userToRegister) {
+        UserDao userDao = new UserDao();
+        User retrievedUser = userDao.findByUsername(userToRegister.getUsername());
+        return userToRegister.getUsername().equals(retrievedUser.getUsername());
     }
 
     private Cookie getCookie(Cookie[] cookies, String name) {
