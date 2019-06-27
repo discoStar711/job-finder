9dadaff6f2417b3146d5713da93e2c324bae70d2
me <discostar711@gmail.com>
2019-06-27 18:37:15 +0100

    Add save() method to save jobs in database

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/JobManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/JobManager.java
@@ -1,6 +1,12 @@
 package com.rajewski.jobfinder.webapp.find;
 
+import java.util.List;
+
 public class JobManager
 {
-
+    private void save(List<GoogleJob> jobs)
+    {
+        JobDao jobDao = new JobDao();
+        jobDao.saveAll(jobs);
+    }
 }
