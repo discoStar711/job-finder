25f7d2077a1713bc03830cd31571636fa6008062
me <discostar711@gmail.com>
2019-06-27 17:24:47 +0100

    Add data source

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/JobDao.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/JobDao.java
@@ -1,6 +1,18 @@
 package com.rajewski.jobfinder.webapp.find;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
 public class JobDao
 {
-
+    public DataSource dataSource()
+    {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/jobs?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
 }
