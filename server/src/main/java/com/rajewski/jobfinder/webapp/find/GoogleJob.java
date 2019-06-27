884fb16f875a00acfed612322db4f225cd79a370
me <discostar711@gmail.com>
2019-06-27 18:32:02 +0100

    Add field to extend parent class

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/GoogleJob.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/GoogleJob.java
@@ -2,5 +2,15 @@ package com.rajewski.jobfinder.webapp.find;
 
 public class GoogleJob extends Job
 {
-    
+    private Integer technologyId;
+
+    public Integer getTechnologyId()
+    {
+        return technologyId;
+    }
+
+    public void setTechnologyId(Integer technologyId)
+    {
+        this.technologyId = technologyId;
+    }
 }
