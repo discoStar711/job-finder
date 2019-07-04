f5372054b0ab8e09b4e0917a6bef5901799510be
me <discostar711@gmail.com>
2019-07-04 18:55:34 +0100

    Add constant fields to represent API request and URL parameters

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/request/GoogleRequest.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/request/GoogleRequest.java
@@ -2,5 +2,7 @@ package com.rajewski.jobfinder.webapp.find.request;
 
 public class GoogleRequest
 {
-
+    private static final String API_URL = "https://www.googleapis.com/customsearch/v1?";
+    private static final String API_KEY = "key=";
+    private static final String ID = "cx=";
 }
