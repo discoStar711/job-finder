bf0a5b5b6b5258c08c33aaacee7dece86c880350
me <discostar711@gmail.com>
2019-07-04 14:14:43 +0100

    Add method to delegate finding jobs

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/JobManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/JobManager.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class JobManager
 {
+    private void find()
+    {
+        try
+        {
+            JobFinder finder = new JobFinder();
+            List<GoogleJob> jobs = finder.find();
+            save(jobs);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
     private void save(List<GoogleJob> jobs)
     {
         JobDao jobDao = new JobDao();
