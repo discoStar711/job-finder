af5f150e061558d42ea82771cdbacbeb5da250b1
me <discostar711@gmail.com>
2019-07-04 17:53:39 +0100

    Add method to set schedule for next job fetch

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/JobManager.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/JobManager.java
@@ -1,11 +1,26 @@
 package com.rajewski.jobfinder.webapp.find;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class JobManager
 {
     private static final long HOUR = 1000 * 60 * 60;
     private static final long TWELVE_HOURS = 12 * HOUR;
+
+    private void scheduleNextJobFetch()
+    {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                
+            }
+        }, TWELVE_HOURS);
+    }
 
     private void find()
     {
