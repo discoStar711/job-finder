19e526b855d1b0d217aa707c2ca3448485347783
me <discostar711@gmail.com>
2019-07-12 23:13:11 +0100

    Add contructor to ensure data is injected into object's fields

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/request/GoogleRequest.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/request/GoogleRequest.java
@@ -1,8 +1,25 @@
 package com.rajewski.jobfinder.webapp.find.request;
 
+import java.util.Map;
+
 public class GoogleRequest
 {
     private static final String API_URL = "https://www.googleapis.com/customsearch/v1?";
     private static final String API_KEY = "key=";
     private static final String ID = "cx=";
+
+    private Map<String, Object> jobProvider;
+    private Map<String, Object> position;
+    private Map<String, Object> technology;
+
+    public GoogleRequest(
+            Map<String, Object> jobProvider,
+            Map<String, Object> position,
+            Map<String, Object> technology
+    )
+    {
+        this.jobProvider = jobProvider;
+        this.position = position;
+        this.technology = technology;
+    }
 }
