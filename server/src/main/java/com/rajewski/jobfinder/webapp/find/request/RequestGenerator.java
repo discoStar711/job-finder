6ca6faaa48168bd43f727120712e68c7cc2b9d07
me <discostar711@gmail.com>
2019-07-20 19:51:33 +0100

    Iterate lists of search parameters to get all cases

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/request/RequestGenerator.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/request/RequestGenerator.java
@@ -1,5 +1,6 @@
 package com.rajewski.jobfinder.webapp.find.request;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +12,18 @@ public class RequestGenerator
             List<Map<String, Object>> technologies
     ) throws Exception
     {
+        List<Map<String, Object>> responseList = new LinkedList<>();
 
+        for (Map<String, Object> jobProvider : jobProviders)
+        {
+            for (Map<String, Object> position : positions)
+            {
+                for (Map<String, Object> technology : technologies)
+                {
+                    
+                }
+            }
+        }
+        return responseList;
     }
 }
