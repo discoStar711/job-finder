c3c1a43d3911c12abe41f4a2aad86a6c7aadc68b
me <discostar711@gmail.com>
2019-07-20 19:46:38 +0100

    Add method to return jobs fetched from Google API

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/request/RequestGenerator.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/request/RequestGenerator.java
@@ -1,6 +1,16 @@
 package com.rajewski.jobfinder.webapp.find.request;
 
+import java.util.List;
+import java.util.Map;
+
 public class RequestGenerator
 {
+    public List<Map<String, Object>> getJobsFromGoogleBy(
+            List<Map<String, Object>> jobProviders,
+            List<Map<String, Object>> positions,
+            List<Map<String, Object>> technologies
+    ) throws Exception
+    {
 
+    }
 }
