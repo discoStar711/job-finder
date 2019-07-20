05397e9760618947f1ef03988613debb9f75db10
me <discostar711@gmail.com>
2019-07-20 23:51:57 +0100

    Add method to get formatted data as list of jobs from Google API

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/response/ResponseGenerator.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/response/ResponseGenerator.java
@@ -1,7 +1,17 @@
 package com.rajewski.jobfinder.webapp.find.response;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class ResponseGenerator
 {
+    public List<GoogleJob> getGoogleJobList(List<Map<String, Object>> data)
+    {
+        List<GoogleJob> list = new LinkedList<>();
+        return list;
+    }
+
     private String escapeSingleQuote(String text)
     {
         return text.replace("'", "''");
