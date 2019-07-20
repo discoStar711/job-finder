b423ae93b60a86f9effa24867bb68c6417d1c2ef
me <discostar711@gmail.com>
2019-07-21 00:11:28 +0100

    Parse JSON and map values to GoogleResponse data structure

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/response/ResponseGenerator.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/response/ResponseGenerator.java
@@ -1,5 +1,8 @@
 package com.rajewski.jobfinder.webapp.find.response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rajewski.jobfinder.webapp.find.response.google.GoogleResponse;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +19,16 @@ public class ResponseGenerator
             Integer positionId = (Integer) job.get("experience_id");
             Integer providerId = (Integer) job.get("provider_id");
             Integer technologyId = (Integer) job.get("technology_id");
+
+            try
+            {
+                ObjectMapper mapper = new ObjectMapper();
+                GoogleResponse response = mapper.readValue(json, GoogleResponse.class);
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
         }
         return list;
     }
