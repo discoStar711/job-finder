7b1b317500621bbfaa6e9558ade890fd6964b04c
me <discostar711@gmail.com>
2019-07-20 23:38:48 +0100

    Add method to escape single quote in string to prevent errors in SQL queries

--- server/src/main/java/com/rajewski/jobfinder/webapp/find/response/ResponseGenerator.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/find/response/ResponseGenerator.java
@@ -2,5 +2,8 @@ package com.rajewski.jobfinder.webapp.find.response;
 
 public class ResponseGenerator
 {
-
+    private String escapeSingleQuote(String text)
+    {
+        return text.replace("'", "''");
+    }
 }
