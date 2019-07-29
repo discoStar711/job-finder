8e8587c57dc9e0b20c8b84e53b68278b18622528
me <discostar711@gmail.com>
2019-07-29 21:57:25 +0100

    Check if authenticated principal has value in CSRF token property

--- server/src/main/java/com/rajewski/jobfinder/webapp/security/api/ApiRequestAuthenticationSuccessHandler.java
+++ server/src/main/java/com/rajewski/jobfinder/webapp/security/api/ApiRequestAuthenticationSuccessHandler.java
@@ -1,9 +1,12 @@
 package com.rajewski.jobfinder.webapp.security.api;
 
+import com.rajewski.jobfinder.webapp.security.CsrfTokenManager;
+import com.rajewski.jobfinder.webapp.user.UserSessionManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,6 +20,13 @@ public class ApiRequestAuthenticationSuccessHandler implements AuthenticationSuc
             Authentication authentication
     ) throws IOException, ServletException
     {
+        ApiRequestAuthenticationToken authenticatedPrincipal = (ApiRequestAuthenticationToken) authentication;
+
+        if (authenticatedPrincipal.getCsrfToken() != null)
+        {
+            
+        }
+
         request.getRequestDispatcher(request.getRequestURI()).forward(request, response);
     }
 }
